85ed893acb1b15f7731b208ca870e92002fada64
ekilo <43447459+ekilo@users.noreply.github.com>
2021-11-24 20:52:07 +0100

    Add project files.

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly Data.AplicationDbContext _db;
+        public CategoryController(Data.AplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Models.Category> objList = _db.Categories;
+            return View(objList);
+        }
+    }
+}
---
+++ Data/AplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Data
+{
+    public class AplicationDbContext:DbContext
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options) : base(options)
+        {
+        }
+            public DbSet<Models.Category> Categories { get; set; }
+    
+    }
+}
